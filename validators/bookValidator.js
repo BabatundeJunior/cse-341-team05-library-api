41a2e5892b9dd54dd3ff2362e70ae00f6bc2df03
BabatundeJunior <jbabs128@gmail.com>
2025-10-13 15:58:28 +0100

    book validation updated

--- validators/bookValidator.js
+++ validators/bookValidator.js
@@ -3,13 +3,9 @@ const { body, param } = require('express-validator');
 exports.validateBook = [
   body('title').notEmpty().withMessage('Title is required'),
   body('isbn').notEmpty().withMessage('ISBN is required'),
-  body('publishDate')
-    .isInt({ min: 1000, max: 9999 })
-    .withMessage('Published year must be a 4-digit number'),
+  body('publishDate').notEmpty().withMessage('Published date is required'),
   body('genre').notEmpty().withMessage('Genre is required'),
-  body('copiesAvailable')
-    .isInt({ min: 0 })
-    .withMessage('Copies available must be a non-negative integer'),
+  body('copiesAvailable').notEmpty().withMessage('Copies available is required'),
   body('authorId').isMongoId().withMessage('Valid author ID is required')
 ];
 
